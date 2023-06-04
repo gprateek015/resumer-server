37cee423a62367ad181f7bdd0ce323effb9389f6
jainPrabhanshu <prabhanshurajnama@gmail.com>
2023-06-04 22:26:01 +0530

    Added new template

--- resume/templates/engineering/index.js
+++ resume/templates/engineering/index.js
@@ -1,7 +1,9 @@
 import template1 from './template1.js';
+import template2 from './template2.js';
 
 const engineeringTemplates = {
-  0: template1
+  0: template1,
+  1: template2
 };
 
 export default engineeringTemplates;
